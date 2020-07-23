e07a4eebe61e113ce57bcd4153bcb048e0240fb8
Ricardo Pacheco <rpacheco@cloudflare.com>
2020-07-23 01:40:28 +0100

    Fixed a bug where indexes were not boolean

--- decision-trees/python/utils.py
+++ decision-trees/python/utils.py
@@ -8,8 +8,8 @@ def to_array(x):
 
 
 def is_left(x, boundary):
-    return (to_array(x) <= boundary).astype(int)
+    return (to_array(x) <= boundary).astype(bool)
 
 
 def is_right(x, boundary):
-    return (to_array(x) > boundary).astype(int)
+    return (to_array(x) > boundary).astype(bool)
