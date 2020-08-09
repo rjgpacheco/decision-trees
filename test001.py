4a2595a3933c2920b895c6ac1994ab2b23ac545b
Ricardo Pacheco <rjgpacheco@gmail.com>
2020-08-09 22:52:38 +0100

    Added small test

---
+++ test001.py
@@ -0,0 +1,22 @@
+import sys
+import pytest
+from sklearn import datasets
+import numpy as np
+
+sys.path.append("../../decision-trees/python")
+
+from decision_trees import DecisionNode
+
+node = DecisionNode()
+
+iris = datasets.load_iris()
+
+X = iris["data"]
+y = iris["target"]
+y = (y == 0).astype(int)
+
+x0 = X[:, 0]
+
+node.fit_node(x0, y, indexes=np.ndarray([]))
+
+print(node)
