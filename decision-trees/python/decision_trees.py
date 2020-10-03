5e4e4ebcc2dba62e72044e72a7937ccd6674ce04
Ricardo Pacheco <rjgpacheco@gmail.com>
2020-10-03 03:20:29 +0100

    Removed unused imports

--- decision-trees/python/decision_trees.py
+++ decision-trees/python/decision_trees.py
@@ -1,13 +1,2 @@
-import json  # NOTE: this is here just so that the str() method is pretty
-import warnings
-import random
-
-import numpy as np
-
-from utils import is_left, is_right
-from uuid import uuid4
-
-from logger import logger
-
 from decision_node import DecisionNode
 from decision_tree import DecisionTree
