992aac39c6d09ac4761c17a9902345dba648e69d
Ricardo Pacheco <rjgpacheco@gmail.com>
2020-10-03 03:10:04 +0100

    resolves #11: Add seperate logger file (#13)

--- tests/pytest/decison_node.py
+++ tests/pytest/decison_node.py
@@ -8,18 +8,11 @@ sys.path.append("../../decision-trees/python")
 from decision_trees import DecisionNode
 from decision_trees import DecisionTree
 
-node = DecisionNode()
-
 iris = datasets.load_iris()
 
 X = iris["data"]
 y = iris["target"]
 y = (y == 0).astype(int)
 
+node = DecisionNode()
 node.fit(X, y, indexes=np.ndarray([]))
-
-
-# Tree
-
-tree = DecisionTree().fit(X, y)
-print(tree)
---
+++ tests/pytest/decison_tree.py
@@ -0,0 +1,18 @@
+import sys
+import pytest
+from sklearn import datasets
+import numpy as np
+
+sys.path.append("../../decision-trees/python")
+
+from decision_trees import DecisionNode
+from decision_trees import DecisionTree
+
+iris = datasets.load_iris()
+
+X = iris["data"]
+y = iris["target"]
+y = (y == 0).astype(int)
+
+tree = DecisionTree()
+tree.fit(X, y)
