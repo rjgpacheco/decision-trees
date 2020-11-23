90085e6efd9fa5e0f800998f7660f68f90cda21d
Ricardo Pacheco <rjgpacheco@gmail.com>
2020-11-23 00:41:18 +0000

    Modifications for cross-platform compatibility

--- decision-trees/python/logger.py
+++ decision-trees/python/logger.py
@@ -1,5 +1,5 @@
 import logging
 
 FORMAT = "%(asctime)-15s %(message)s"
-logging.basicConfig(filename="/home/ricardo/repos/decision-trees/decision_trees.log", level=logging.INFO, format=FORMAT)
+logging.basicConfig(filename="decision_trees.log", level=logging.INFO, format=FORMAT)
 logger = logging.getLogger("DecisionTrees")
